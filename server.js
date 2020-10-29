dfa8c99929df8dc2d2e6ce5f4f41d274d2424559
ThallesAguiar <thallesaguiar@github.com>
2020-10-29 18:06:34 -0300

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require(express);
+var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 
 var app = express();
 
-app.use('/',serveStatic(path.join(__dirname,'dist')));
+app.use(serveStatic(path.join(__dirname,'dist')));
 
 var port = process.env.PORT || 8080;
-app.listen(post);
-console.log('server started' + port);
+app.listen(port);
+console.log('server started ' + port);
